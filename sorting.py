379a0467550dfbb0f6fa6616917da25dae92b358
Jean Liu <hjliu415@gmail.com>
2016-09-03 17:40:27 -0700

    Done

---
+++ sorting.py
@@ -0,0 +1,83 @@
+#Sorting
+
+def bubble_sort(lst):
+    """returns a sorted list using a optimized bubble sort algorithm
+    i.e. using a variable to track if there hasn't been a swap
+        >>> bubble_sort([3, 5, 7, 2, 4, 1])
+        [1, 2, 3, 4, 5, 7]
+    """
+    
+    for big_i in range(len(lst) - 1):
+        swapped = False
+        for i in range(len(lst) - 1 - big_i):
+            if lst[i] > lst[i+1]:
+                lst[i], lst[i+1] = lst[i+1], lst[i]
+                swapped = True
+        if not swapped:
+            break
+
+    return lst
+
+def merge_lists(list1, list2):
+    """Given two sorted lists of integers, returns a single sorted list containing all
+    integers in the input lists
+    >>> merge_lists([1, 3, 9], [4, 7, 11])
+    [1, 3, 4, 7, 9, 11]
+    """
+
+    third_list = []
+
+    while list1 or list2:
+        if list1 == []:
+            third_list.append(list2.pop(0))
+        elif list2 == []:
+            third_list.append(list1.pop(0))
+        elif list1[0] < list2[0]:
+            third_list.append(list1.pop(0))
+        else:
+            third_list.append(list2.pop(0))
+
+    return third_list
+
+##########ADVANCED##########
+def merge_sort(lst):
+    """
+    Given a list, returns a sorted version of that list.
+    Finish the merge sort algorithm by writing another function that
+    that takes in a single unsorted list of integers and uses recursion and the 'merge_lists'
+    function you already wrote to return a new sorted list containing all integers from
+    thin input list. In other words, the new function should sort a list using merge_lists
+    and recursion.
+    >>> merge_sort([6, 2, 3, 9, 0, 1])
+    [0, 1, 2, 3, 6, 9]
+    """
+
+    if len(lst) < 2:
+        return lst
+
+    midway = int(len(lst))/2
+    list1 = merge_sort(lst[:midway])
+    list2 = merge_sort(lst[midway:])
+
+    return merge_lists(list1, list2)
+
+
+
+
+
+
+
+
+
+
+#####################################################################
+# END OF ASSIGNMENT: You can ignore everything below.
+
+if __name__ == "__main__":
+    import doctest
+
+    print
+    result = doctest.testmod()
+    if not result.failed:
+        print "ALL TESTS PASSED. GOOD WORK!"
+    print
